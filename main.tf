01d42db360998f5af970909af7ea2a668a52ef52
Cholkata <114622501+Cholkata@users.noreply.github.com>
2025-04-10 09:24:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,23 +4,7 @@ terraform {
     key    = "key/terraform-statefile.tflock"
     region = "eu-north-1"
   }
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.0"
-    }
-  }
 }
-
 provider "aws" {
   region = "eu-north-1"
-}
-
-resource "aws_instance" "example" {
-  ami = "ami-0c1ac8a41498c1a9c"
-  instance_type = "t3.micro"
-  
-  tags = {
-    Name = "terraform-example"
-  }
 }
